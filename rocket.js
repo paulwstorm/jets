de52545fa77dc86dd1aa65e5fe10eaf1b4723027
paulwstorm <anapaulorocket@gmail.com>
2020-02-19 14:23:45 -0500

    Added add_takeoff function

--- rocket.js
+++ rocket.js
@@ -1,6 +1,6 @@
 var Rocket = function (color, size) {
-    this.color = color;
-    this.size = size;
-  }
-  
-  module.exports = Rocket;
+  this.color = color;
+  this.size = size;
+}
+
+module.exports = Rocket;
